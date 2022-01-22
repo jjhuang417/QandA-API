af88864a985411bce2e8ca157a6a105cf44109b0
Jesse Huang <jjhuang417@gmail.com>
2022-01-22 09:38:12 -0800

    Adding in loaderio key.

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const router = require('./router.js');
+const TOKEN = require('../loaderio.js');
 
 
 app.use(express.json());
@@ -11,6 +12,9 @@ app.use(express.json());
 // Request Handling
 // *************
 app.use('/qa', router);
+app.get(`/${TOKEN}`, (req, res) => {
+  res.status(200).send(`${TOKEN}`)
+});
 
 // *************
 // Server
